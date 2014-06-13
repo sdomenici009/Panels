e0e1a87fab243ecf53222446a9bc238170e1e39e
Stephen Domenici <sdomenici009@gmail.com>
2014-06-12 20:56:42 -0700

    Added Grid and Panels

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flip : MonoBehaviour {
+	
+	float rotation = .1f;
+	float orginTime = 1f;
+	float timer;
+	bool flip = false;
+	// Use this for initialization
+	void Start () {
+		timer = orginTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		
+		if(flip)
+		{
+			transform.rotation = new Quaternion(Mathf.Clamp(transform.rotation.x + rotation, 0, 360), transform.rotation.y, transform.rotation.z, transform.rotation.w);
+		}
+		
+		if(timer < 0)
+		{
+			timer = orginTime;
+			
+			if(flip)
+			{
+				flip = false;
+			}
+			else
+			{
+				flip = true;
+				rotation = -rotation;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+	
+	Vector2 gridSize = Vector2.one*7;
+	float spacing = 1.5f;
+	
+	// Use this for initialization
+	void Start () {		
+		float camHalfHeight = Camera.main.orthographicSize;
+    	float camHalfWidth = Camera.main.aspect * camHalfHeight; 
+		
+		Vector3 topLeftPosition = new Vector3(-camHalfWidth, camHalfHeight, 0) + Camera.main.transform.position; 
+		
+		topLeftPosition += new Vector3(1 / 2, -1 / 2, 0);
+				
+		for(int i=0; i < gridSize.x; i++)
+		{
+			for(int j=0; j < gridSize.y; j++)
+			{
+				Instantiate(Resources.Load("Panel"), new Vector3(topLeftPosition.x + i*spacing, topLeftPosition.y - j*spacing, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
